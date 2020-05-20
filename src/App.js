b4bedaa1f0627100db740cfdcd8aa2af31f98409
Javier Garcia Menendez <javiergme@servicioexterno.inditex.com>
2020-05-20 17:51:42 +0200

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function Hello(props) {
+//   return <h2>{ props.title }</h2>
+// }
+
+// const Hello = (props) => <h2>{ props.title }</h2>
+
+class Hello extends Component {
+  render() {
+    return <h2>{ this.props.title }</h2>
+  }
+}
+
+class Text extends Component {
+  render() {
+    const { arrayOfNumbers, multiply, objectWithInfo, title } = this.props;
+    const mappedNumbers = arrayOfNumbers.map(multiply);
+
+    return (
+      <div>
+        { title }
+        <p>{ mappedNumbers.join(', ') }</p>
+        <p>{ objectWithInfo.key }</p>
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Hello title="Hello from props" />
+        <Text 
+          arrayOfNumbers={ [2, 3, 10] }
+          multiply={ (number) => number * 3 }
+          number={ 2 } 
+          objectWithInfo={{ key: 'value', key2: 'otherValue' }}
+          title={ <h1>Este es el titulo</h1> }
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
