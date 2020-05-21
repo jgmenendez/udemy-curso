11b74ac637752c8b3cbd848e4334e9d79bc73153
Javier Garcia Menendez <javiergme@servicioexterno.inditex.com>
2020-05-21 13:36:45 +0200

    Eventos

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React, { Component } from "react";
-import cars from './data/cars.json';
-
-class CarItem extends Component {
-  render() {
-    const { car } = this.props;
-
-    return (
-      <li>
-        <p><strong>Nombre: </strong>{ car.name }</p>
-        <p><strong>Marca: </strong>{ car.company }</p>
-      </li>
-    )
-  }
-}
 
 class App extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = { mouseX: 0, mouseY: 0 };
+    // this.handleMouseMove = this.handleMouseMove.bind(this);
+  }
 
+  handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    this.setState({ mouseX: clientX, mouseY: clientY });
+  }
+
+  handleClick(e) {
+    console.log(e);
+    console.log(e.nativeEvent);
+    alert('Hi here!');
+  }
+
+  render() {
     return (
       <div className="App">
-        <h4>Trabajando con listas con objetos</h4>   
-        <ul>
-          {
-            cars.map(car => {
-              return <CarItem key={ car.id } car={ car } />
-            })
-          }
-        </ul>
+        <h4>Eventos</h4>   
+        <button onClick={ this.handleClick }>Hi there!</button>
+        <div 
+          onMouseMove={ this.handleMouseMove }
+          style={{ border: '1px solid #000', marginTop: 10, padding: 10 }}>
+          <p>{ this.state.mouseX }, { this.state.mouseY }</p>
+        </div>
       </div>
     );
   }
