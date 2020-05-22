d1921f265b1d6ef29e47c68fd94b5dd8901acd75
Javier Garcia Menendez <javiergme@servicioexterno.inditex.com>
2020-05-22 12:23:47 +0200

    Fetch API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import EjemploDeComponentDidMount from './sections/life-cycle/componentDidMount';
+import FetchExample from './sections/fetch-example';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <EjemploDeComponentDidMount />
+        <FetchExample />
       </div>
     );
   }
