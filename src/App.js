e933b0d1906b6e921cacd14c334c8c5de5c6a97f
Javier Garcia Menendez <javiergme@servicioexterno.inditex.com>
2020-05-22 10:54:48 +0200

    ComponentWillMount

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import React, { Component } from "react";
+import ComponentWillMount from './sections/life-cycle/componentWillMount'
 
 class App extends Component {
-  // constructor por defecto
-  // constructor(...args) {
-  //   super(...args);
-  // }
-
-  constructor(props) {
-    console.log('constructor');
-    super(props); // Este método llama al constructor de Component
-
-    // inicializamos el state de nuestro componente
-    this.state = { mensajeInicial: 'mensaje inicial' };
-
-    // bindemaos el contexto al método
-    // this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick = () => {
-    this.setState({ mensajeInicial: 'mensaje cambiado' });
-  }
-
   render() {
-    console.log('render');
     return (
       <div className="App">
-        <h4>Ciclo de montaje: constructor</h4>
-        { this.state.mensajeInicial }
-        <button onClick={ this.handleClick }>Cambiar mensaje</button>
+        <ComponentWillMount />
       </div>
     );
   }
