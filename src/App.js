c84d3545f7fd9ce247a4c20a965f7469275c7fb8
Javier Garcia Menendez <javiergme@servicioexterno.inditex.com>
2020-05-25 11:32:42 +0200

    ComponentWillUnmount

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import EjemploCicloDeActualizacion from './sections/life-cycle/ejemploCicloDeActualizacion';
+import EjemploDeComponentWillUnmount from './sections/life-cycle/componentWillUnmount';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <EjemploCicloDeActualizacion />
+        <EjemploDeComponentWillUnmount />
       </div>
     );
   }
